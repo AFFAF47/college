b888205131a5e4f3f203e59cf11c8512a4b715b6
MOHAMMED ANAS AFFAF <anasaffaf@gmail.com>
2023-10-09 14:10:38 +0530

    Project Started

---
+++ elasticsearch-project/src/main/java/com/example/elasticsearch/elasticsearchproject/Models/Student.java
@@ -0,0 +1,14 @@
+package com.example.elasticsearch.elasticsearchproject.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Document(collection = "students")
+public class Student {
+
+}
