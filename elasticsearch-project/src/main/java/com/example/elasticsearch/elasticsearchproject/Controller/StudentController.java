09d685b4256343334d5b6ba792e91ad4f5dd5556
MOHAMMED ANAS AFFAF <anasaffaf@gmail.com>
2023-10-09 14:36:25 +0530

    Get and Post of student is done

--- elasticsearch-project/src/main/java/com/example/elasticsearch/elasticsearchproject/Controller/StudentController.java
+++ elasticsearch-project/src/main/java/com/example/elasticsearch/elasticsearchproject/Controller/StudentController.java
@@ -3,10 +3,25 @@ package com.example.elasticsearch.elasticsearchproject.Controller;
 import com.example.elasticsearch.elasticsearchproject.Models.Student;
 import com.example.elasticsearch.elasticsearchproject.Services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/students")
+@CrossOrigin(origins = "*")
 public class StudentController {
     @Autowired
     private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getStudents();
+    }
+
+    @PostMapping("/addstudent")
+    public Student addStudent(@RequestBody Student student){
+        return studentService.addStudent(student);
+    }
+
 }
